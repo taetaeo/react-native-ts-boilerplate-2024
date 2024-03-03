6bfb1673c4b1e3f8a1cb99cf32231974f0388e76
ohtaekwon <ohtk1119@gmail.com>
2024-03-04 08:34:18 +0900

    feat: add node version file and write v.18

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  printWidth: 180, // 한 줄에 출력되는 코드의 최대 길이
-  tabWidth: 2, // 탭 간격의 크기를 설정
-  arrowParens: 'avoid', // 화살표 함수의 매개변수가 하나일 때 괄호를 생략하도록 설정
-  bracketSameLine: true, // 객체 리터럴의 중괄호를 같은 줄에 표시하도록 설정
-  bracketSpacing: false, // 중괄호 주위에 공백을 추가하지 않도록 설정
-  singleQuote: true, //  문자열을 작은 따옴표로 표시
-  trailingComma: 'all', // 후행 쉼표를 모든 요소에 적용
-  semi: false, // JavaScript 코드에서 세미콜론 (;)을 사용할지 여부를 설정하는 옵션
+  printWidth: 180, // 한 줄에 출력되는 코드의 최대 길이 ( 180 : 최대길이 180)
+  tabWidth: 2, // 탭 간격의 크기를 설정 ( 2 :각 탭이 2개의 공백)
+  arrowParens: 'avoid', // 화살표 함수의 매개변수가 하나일 때 괄호를 생략하도록 설정 ( 'avoid' : 가능한 경우 괄호를 피하도록 설정)
+  bracketSameLine: true, // 객체 리터럴의 중괄호를 같은 줄에 위치 여부 ( true : 같은 줄에 위치 시키지 않음)
+  bracketSpacing: false, // 중괄호 주위에 공백을 추가에 대한 설정 ( false : 추가하지 않음)
+  singleQuote: true, //  문자열을 작은 따옴표로 표시 ( true : 문자열은 작은 따옴표 표시)
+  trailingComma: 'all', // 객체, 배열 등의 마지막 항목 뒤에 쉼표를 모든 요소에 적용 ( 'all' : 항상 쉼표를 적용 )
+  semi: false, // JavaScript 코드에서 문장의 끝에 세미콜론 (;)을 사용할지 여부를 설정하는 옵션  ( false : 세미콜론을 사용하지 않음)
 }
